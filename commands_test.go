4e40af0d535716ef4fde13b3c88a87a825c9eed2
Lorenzo Allotta <lorenzo.allotta@gmail.com>
2019-09-05 13:27:39 -0400

    Condense tests

--- commands_test.go
+++ commands_test.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 )
-
-func TestInvalidCommand(t *testing.T) {
-	input := "foo"
-	err := runCommand(input, []string{})
-	want := fmt.Sprintf("invalid command: '%s'", input)
-
-	if err == nil {
-		t.Errorf("'%s' ran successfully", input)
-	} else {
-		if !strings.Contains(err.Error(), want) {
-			t.Errorf("%s: got %q; want %q", input, err, want)
-		}
-	}
-}
 
 func TestBadCommandLine(t *testing.T) {
 	tests := []struct {
@@ -28,17 +13,16 @@ func TestBadCommandLine(t *testing.T) {
 		{"search", "no search terms specified"},
 		{"info", "no packages specified"},
 		{"get", "no targets specified"},
+		{"foo", "invalid command: 'foo'"},
 	}
 
 	for _, test := range tests {
 		t.Run(test.in, func(t *testing.T) {
 			err := runCommand(test.in, []string{})
 			if err == nil {
-				t.Errorf("'%s' ran successfully with no arguments", test.in)
-			} else {
-				if !strings.Contains(err.Error(), test.want) {
-					t.Errorf("%s: got %q; want %q", test.in, err, test.want)
-				}
+				t.Errorf("'%s' ran successfully", test.in)
+			} else if !strings.Contains(err.Error(), test.want) {
+				t.Errorf("%s: got %q; want %q", test.in, err, test.want)
 			}
 		})
 	}
